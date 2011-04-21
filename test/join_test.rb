bca164a8f5dbb950fd486dc6865d00638339235d
Aaron Hamid <aaron.hamid@gmail.com>
2011-04-21 02:44:37 -0400

    some refactoring

--- test/join_test.rb
+++ test/join_test.rb
@@ -7,30 +7,42 @@ class CSVFile
 
   def initialize(file)
     raise "Invalid file" unless File.file?(file)
-    @io = File.open(file, 'r')
-    @csv = CSV::new(@io, { :converters => :numeric })
+    @file = file
+    @header = csv_load(file).first
+  end
+
+  def data
+    csv = csv_load(@file).last
+    csv.read 
+    #csv_enumerator(csv)
+  end
+
+  protected
+
+  def csv_load(file)
+    io = File.open(file, 'r')
+    csv = CSV::new(io, { :converters => :numeric })
     # read the header into Veritas::Relation header array
-    @header = @csv.shift.map { |col|
+    header = csv.shift.map { |col|
       parts = col.split(':')
       # key, class
       [ parts[0].downcase.to_sym, Kernel.const_get(parts[1]) ]
     }
-    @data = @csv.read
+    return header, csv
   end
 
-  def data
-    # @csv.enum_for(:shift) # doesn't work???
-    #Enumerator.new do |y|
-    #  loop {
-    #    v = @csv.shift 
-    #    if v
-    #      y << v
-    #    else
-    #      break
-    #    end
-    #  }
-    #end
-    @data
+  def csv_enumerator(csv)
+    # cvs.enum_for(:shift) ??
+    Enumerator.new do |y|
+      loop {
+        v = csv.shift 
+        if v
+          y << v
+        else
+          break
+        end
+      }
+    end
   end
 end
 
@@ -45,12 +57,12 @@ class JoinTest < Test::Unit::TestCase
     p "theta join"
     renamed = pet_relation.rename(:name => :pet_name, :id => :pet_id)
     # select * from owner, pet where owner.id = pet.owner and pet.type = 'Cat'
-    owner_relation.join(renamed, owner_relation[:id].eq(renamed[:owner])).restrict(renamed[:type].eq('Cat')).each do |r|
+    owner_relation.join(renamed, owner_relation[:id].eq(renamed[:owner])).restrict { |rel| rel[:type].eq('Cat') }.each do |r|
       p r
     end
 
     p "natural join"
-    owner_relation.join(pet_relation.rename(:id => :pet_id, :name => :pet_name, :owner => :id)).restrict(pet_relation[:type].eq('Cat')).each do |r|
+    owner_relation.join(pet_relation.rename(:id => :pet_id, :name => :pet_name, :owner => :id)).restrict { |rel| rel[:type].eq('Cat') }.each do |r|
       p r
     end
   end
