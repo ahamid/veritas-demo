53b304ae2dd37123bb21f2646f4364f86b429e8f
Aaron Hamid <aaron.hamid@gmail.com>
2011-04-21 00:05:47 -0400

    simpler fib

--- test/dynamic_results_test.rb
+++ test/dynamic_results_test.rb
@@ -4,28 +4,21 @@ require 'veritas'
 class DynamicResultsTest < Test::Unit::TestCase
   def test_dynamic_results
     g = generative_fib
-    relation = Veritas::Relation.new([ [ :num, Integer ] ], g)
+    relation = Veritas::Relation.new([ [ :num, Integer ] ], )
     # does not load all values into memory
-    divisible_by_5 = relation.restrict(lambda { |tuple| tuple[:num] % 5 == 0}).take(20)
-    divislbe_by_5.each { |v| p v }
+    divisible_by_5 = relation.restrict(lambda { |tuple| tuple[:num] % 5 == 0}).order([ relation[:num] ]).take(20)
+    divisible_by_5.each { |v| p v }
   end
 
   protected
 
   def generative_fib
-    Enumerator.new { |b|
-      history = [ ]
-      while true do
-        if history.empty?
-          b.yield 0 
-          history += [ 0, 1]
-        else
-          val = history[0] + history[1]
-          history.shift
-          history << val
-          b.yield val
-        end
-      end
+    Enumerator.new { |y|
+      a = b = 1
+      loop {
+        y << a
+        a, b = b, a + b
+      }
     }
   end
 end
