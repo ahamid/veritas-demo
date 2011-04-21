47fc28b53f353bc3f8c48d2e4c42158f80b58b43
Aaron Hamid <aaron.hamid@gmail.com>
2011-04-21 00:09:51 -0400

    return dynamically generated records

--- test/dynamic_results_test.rb
+++ test/dynamic_results_test.rb
@@ -3,14 +3,21 @@ require 'veritas'
  
 class DynamicResultsTest < Test::Unit::TestCase
   def test_dynamic_results
-    g = generative_fib
-    relation = Veritas::Relation.new([ [ :num, Integer ] ], )
-    # does not load all values into memory
+    relation = Veritas::Relation.new([ [ :num, Integer ] ], generate_records)
     divisible_by_5 = relation.restrict(lambda { |tuple| tuple[:num] % 5 == 0}).order([ relation[:num] ]).take(20)
     divisible_by_5.each { |v| p v }
   end
 
   protected
+
+  def generate_records
+    Enumerator.new { |y|
+      g = generative_fib
+      loop {
+        y << [ g.next ]
+      }
+    }
+  end
 
   def generative_fib
     Enumerator.new { |y|
